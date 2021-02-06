f9ef3c3475d3ce9d7e8f60f606df52f8d401cc2f
Seba-stx <sebastian.sarach@stxnext.pl>
2021-02-06 14:39:18 +0100

    Added credentials to cypress.json. Added selectors and actions for log-in purposes.

--- cypress/integration/end_to_end/log-in.js
+++ cypress/integration/end_to_end/log-in.js
@@ -1,7 +1,18 @@
 describe('Log-in test', () => {
   it('Visits the Kitchen Sink', () => {
+    const username = Cypress.env('username')
+    const password = Cypress.env('password')
+
     cy.visit('https://justjoin.it/')
       // cy.contains('Sign in').click()
-    cy.xpath('//span[text()=\"Sign in\"]').click()
+    cy.xpath('//span[text()="Sign in"]').click()
+    cy.xpath('//span[text()="Sign in as a developer"]').click()
+    cy.xpath('//input[@name="email"]').type(username)
+    cy.xpath('//input[@name="password"]').type(password)
+    cy.xpath('//button[@type="submit"]/span[text()="Sign in"]').click()
+    cy.url()
   })
 })
+
+//sniegolaz9182@mailinator.com
+//BESTcookies18
