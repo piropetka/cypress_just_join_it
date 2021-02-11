8011224c376ccd1251bcd58a49903479a398a294
Seba-stx <sebastian.sarach@stxnext.pl>
2021-02-11 22:02:55 +0100

    Get rid of url

--- cypress/integration/end_to_end/log-in.js
+++ cypress/integration/end_to_end/log-in.js
@@ -1,9 +1,10 @@
 describe('Log-in to justjoin.it test', () => {
-  it('Log-in as a user', () => {
-    const username = Cypress.env('username')
-    const password = Cypress.env('password')
+  const username = Cypress.env('username')
+  const password = Cypress.env('password')
 
-    cy.visit('https://justjoin.it/')
+  it('Log-in as a user', () => {
+
+    cy.visit('/')
     cy.xpath('//span[text()="Sign in"]').click()
     cy.xpath('//span[text()="Sign in as a developer"]').click()
     cy.xpath('//input[@name="email"]').type(username)
@@ -15,7 +16,7 @@ describe('Log-in to justjoin.it test', () => {
 
     cy.request({
       method: 'POST',
-      url: "https://justjoin.it/api/developers/auth",
+      url: "/api/developers/auth",
       form: true,
       body: {
         email: username,
